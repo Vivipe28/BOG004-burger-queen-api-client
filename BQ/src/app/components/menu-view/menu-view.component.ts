b327d42b6f17ca77bdaff9cacd315d603be965b9
unknown <accscortes09@gmail.com>
2022-06-07 13:11:24 -0500

    Creación de grid para mostrar los productos

--- BQ/src/app/components/menu-view/menu-view.component.ts
+++ BQ/src/app/components/menu-view/menu-view.component.ts
@@ -3,8 +3,6 @@ import { Order } from 'src/app/components/models/order';
 import { NotifierService } from 'src/app/services/notifier.service';
 import { AuthService } from '../../services/http.service';
 import { menuService } from '../../services/menu-view.service';
-import { IProductsItem } from '../../components/models/products-item'
-
 
 @Component({
   selector: 'menu-view',
@@ -25,33 +23,14 @@ export class MenuViewComponent implements OnInit {
     private authservice: AuthService, private snackservice: NotifierService,) { }
 
   ngOnInit(): void {
-    // ejemplo de productos
-    // this.ProductsItem = [
-    //   {
-    //     id: 0,
-    //     qty: 0,
-    //     name: 'Café Americano',
-    //     price: 5,
+
+    // this.menuViewService.getMenu().subscribe(
+    //   (res: any) => {
+    //     this.products = res
     //   },
-    //   {
-    //     id: 1,
-    //     qty: 1,
-    //     name: 'Agua',
-    //     price: 500,
-    //   },
-    //   {
-    //     id: 0,
-    //     qty: 1,
-    //     name: 'Gaseosa',
-    //     price: 5000,
-    //   },
-    //   {
-    //     id: 0,
-    //     qty: 1,
-    //     name: 'Gaseosa',
-    //     price: 5000,
-    //   },
-    // ]
+    //   err =>{
+    //     console.log(err);
+    //   })
   }
 
   logOut(){
@@ -63,13 +42,14 @@ export class MenuViewComponent implements OnInit {
     this.menuViewService.deleteOrder(productToDelete)
   }
 
-  getuser(){
-    this.menuViewService.getUser().subscribe(
+  getmenu(){
+    this.menuViewService.getMenu().subscribe(
       (res: any) => {
         this.products = res
-        console.log(res[1].image);
+        this.products.type =res
+        console.log(this.products.type)
       },
-      err =>{
+      err=>{
         console.log(err);
       }
     )
